f1c5896e312b77b052ede9cf3a451c3595b9ae83
Josh Walsh <josh.walsh@the-refinery.io>
2018-01-19 21:47:53 -0500

    Multi-environment setup

--- source/craft/config/db.php
+++ source/craft/config/db.php
@@ -8,20 +8,34 @@
  */
 
 return array(
+	'*' => array(
+		// The prefix to use when naming tables. This can be no more than 5 characters.
+		'tablePrefix' => 'craft',
+	),
+	'localhost' => array(
+		// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+		'server' => 'localhost',
 
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
+		// The database username to connect with.
+		'user' => 'dev',
 
-	// The database username to connect with.
-	'user' => 'craft',
+		// The database password to connect with.
+		'password' => 'dev',
 
-	// The database password to connect with.
-	'password' => 'gisvKkihzV4MGy32WyRHFgJK7ZNNZLPcbGCrZmLDAV6gGHYPqz',
+		// The name of the database to select.
+		'database' => 'oneness',
+	),
+	'discoveroneness.org' => array(
+		// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+		'server' => 'localhost',
 
-	// The name of the database to select.
-	'database' => 'craft',
+		// The database username to connect with.
+		'user' => 'craft',
 
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
+		// The database password to connect with.
+		'password' => 'gisvKkihzV4MGy32WyRHFgJK7ZNNZLPcbGCrZmLDAV6gGHYPqz',
 
+		// The name of the database to select.
+		'database' => 'craft',
+	),
 );
